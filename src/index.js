5d91d36bf48297e8f02fe655f7f8e5458a7fabf4
ahmed-abdellatif <ahmedma@email.sc.edu>
2018-06-23 14:56:29 -0400

    material ui updated

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './pages/index';
+import Home from './pages/home.jsx';
+
+
+ReactDOM.render(<Home />, document.querySelector('#root'));
